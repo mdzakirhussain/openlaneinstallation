2d617936ecac70243eb6c2df85fcae40c9cf52a1
mdzakirhussain <zakir.hussain.work@gmail.com>
2021-07-19 00:06:27 +0530

    Update toolsinstallation.sh

--- toolsinstallation.sh
+++ toolsinstallation.sh
@@ -40,7 +40,7 @@ sudo apt-get install \
     gnupg \
     lsb-release --assume-yes
     
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg --yes
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
